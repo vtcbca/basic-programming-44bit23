596f73ea138079cb3196ae01c7264960f01cd1e4
44bit23 <44bit23@vtcbcsr.edu.in>
2024-12-24 10:58:50 +0530

    Create Assignment4.py

---
+++ Solution/Assignment4.py
@@ -0,0 +1,9 @@
+def reverse_string_recursive(s):
+    if len(s) == 0:
+        return s
+    else:
+        return reverse_string_recursive(s[1:]) + s[0]
+
+# Example usage
+input_str = input("Enter a string: ")
+print(f"Reversed string: {reverse_string_recursive(input_str)}")
